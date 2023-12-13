c048cba3aa8c4f2afdcac93ada572cc4c7315fca
Fabio-Alfredo <00210121@uca.edu.sv>
2023-12-12 18:57:28 -0600

    add: folder order and app.ts

---
+++ API/src/app.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+const PORT = 3000;
+const app = express();
+app.use(cors());
+
+app.listen(PORT, ()=>console.log(`corriendo en el puerto ${PORT}`))
